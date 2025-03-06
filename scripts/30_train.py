7ceba20b35e8517e38fb283d543f66cf66ab0832
Christopher <chrisflex@edeson.com>
2025-03-06 16:41:40 +0100

    Update input and output paths in training script

--- scripts/30_train.py
+++ scripts/30_train.py
@@ -28,8 +28,8 @@ from recur_scan.transactions import group_transactions, read_labeled_transaction
 n_cv_folds = 3  # number of cross-validation folds, could be 5
 n_hpo_iters = 20  # number of hyperparameter optimization iterations
 
-in_path = "your csv file goes here"
-out_dir = "your output directory goes here"
+in_path = "../data/data-fix/updated/updated_transactions.csv"
+out_dir = "../data/output"
 
 # %%
 # parse script arguments from command line
